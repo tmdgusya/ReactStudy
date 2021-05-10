14046eeaf5cdbe6f79c0f91a69c681d81e9b454b
roach <tmdgusya@naver.com>
2021-05-10 22:53:14 +0900

    refactor : 필요없는 파일 제거

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 /**
  * React 는 state 를 props 를 통해서 공유가 가능하다!
@@ -64,7 +63,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : ')');
 
     return (
       <div>
@@ -111,8 +110,3 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
