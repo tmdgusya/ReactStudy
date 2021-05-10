de31af3eea239d8782368591c57ff5cd9036c735
roach <tmdgusya@naver.com>
2021-05-10 23:16:46 +0900

    add  history function in  Game Component

--- src/index.js
+++ src/index.js
@@ -47,36 +47,7 @@ class Board extends React.Component {
       />;
   }
 
-  handleClick(i) {
-    // Copy squares array 
-    // Copy 배열을 쓰는 이유는 상태 변화가 일어날때 마다 render 될 수 있으므로 
-    // 복사 배열을 사용한다.
-    const squares = this.state.squares.slice();
-
-    if(calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-
-    this.setState(
-        {
-          squares : squares,
-          xIsNext : !this.state.xIsNext
-        }
-      )
-  }
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-
-    let status;
-    if(winner) {
-      status = 'Winner : ' + winner;
-    } else { 
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
         <div className="status">{status}</div>
@@ -101,19 +72,73 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      history : [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext : true
+    }
+  }
+
   render() {
+    const history = this.state.history;
+    // currently history
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if(winner) {
+      status = 'Winner : ' + winner;
+    } else { 
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares = {current.squares}
+            onClick = {(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
   }
+
+  handleClick(i) {
+    // Copy squares array 
+    // Copy 배열을 쓰는 이유는 상태 변화가 일어날때 마다 render 될 수 있으므로 
+    // 복사 배열을 사용한다.
+    const history = this.state.history;
+    const current = history[history.length - 1]
+    const squares = current.squares.slice();
+
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    this.setState(
+        {
+          history : history.concat(
+            [{
+              squares : squares
+            }]
+          ),
+          xIsNext : !this.state.xIsNext
+        }
+      )
+  }
+
 }
 
 // ========================================
